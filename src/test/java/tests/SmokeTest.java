80eb5ad2cd8735d60fce238854fbd65d908350da
Rishiraj17 <rajrishi17021709@gmail.com>
2025-12-29 05:25:32 +0530

    Day 1-4: Initial Selenium TestNG framework baseline

---
+++ src/test/java/tests/SmokeTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+import base.BaseTest;
+import org.testng.annotations.Test;
+
+public class SmokeTest extends BaseTest{
+
+    @Test
+    public void openGoogle(){
+        driver.get("https://www.google.com");
+    }
+}
